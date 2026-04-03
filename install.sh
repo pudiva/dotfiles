135cfb73da3cbf58c880ce37590a61d3cc26cb57
Satana de Sant'Ana <satana@skylittlesystem.org>
2026-04-03 15:36:28 +0100

    install: improve a bunch of things

--- install.sh
+++ install.sh
@@ -2,15 +2,27 @@
 # This script assumes coreutils
 set +x
 export VERSION_CONTROL=numbered
-BASE=`realpath --relative-to "$HOME" "`pwd`"`
-ln='ln -sT'
+LNOPTS=""
+BASE="`pwd`"
+
+dotlink() {
+	target="$BASE/$1"
+	link_name="$HOME/$2"
+	mkdir -p "`dirname "$link_name"`"
+	ln $LNOPTS -rsT "$target" "$link_name"
+}
+
+if [ -n "$CODESPACES" ]
+then
+	set -- "-b" "$@" # default to -b on codespaces
+fi
 
 while getopts ":ibf" opt
 do
 	case $opt in
-	i) ln="$ln -i";;
-	b) ln="$ln -b";;
-	f) ln="$ln -f";;
+	i) LNOPTS="$LNOPTS -i";;
+	b) LNOPTS="$LNOPTS -b";;
+	f) LNOPTS="$LNOPTS -f";;
 	\?)
 		echo "Invalid option: -$OPTARG" >&2
 		exit -1
@@ -18,34 +30,29 @@ do
 	esac
 done
 
-if [ -n "$CODESPACES" ]; then
+if [ -n "$CODESPACES" ]
+then
 	sudo apt-get update
 	sudo apt-get install -y neovim fzf ripgrep bat netcat
-	ln="$ln -b"
 
-	# install alacritty terminfo
+	# alacritty terminfo
 	curl -fL 'https://raw.githubusercontent.com/alacritty/alacritty/refs/tags/v0.13.2/extra/alacritty.info' | \
 		tic -xe alacritty,alacritty-direct /dev/stdin
 fi
 
-# shell config
-$ln $BASE/profile			$HOME/.profile
-$ln $BASE/bash_profile			$HOME/.bash_profile
-$ln $BASE/bashrc			$HOME/.bashrc
-$ln $BASE/inputrc			$HOME/.inputrc
-
-# vim config
-mkdir -p $HOME/.config
-$ln $BASE/vim				$HOME/.vim
-$ln $BASE/vim				$HOME/.config/nvim
-$ln .vim/init.vim			$HOME/.vimrc
-
-plug_vim="$BASE/vim/autoload/plug.vim"
+plug_vim="vim/autoload/plug.vim"
 plug_vim_url='https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 if [ ! -f "$plug_vim" ]; then
 	curl -fLo "$plug_vim" --create-dirs "$plug_vim_url" && \
 		nvim +PlugInstall +qa
 fi
 
-# other stuff
-$ln $BASE/alacritty.yml			$HOME/.alacritty.yml
+dotlink profile		.profile
+dotlink bash_profile	.bash_profile
+dotlink bashrc		.bashrc
+dotlink inputrc		.inputrc
+dotlink vim		.config/nvim
+dotlink vim		.vim
+dotlink vim/init.vim	.vimrc
+dotlink alacritty.yml	.alacritty.yml
+dotlink sway		.config/sway
